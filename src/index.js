a68c01260d36b53ca94122667ca042d0033c5737
allentsangdev <allentsang04@gmail.com>
2023-02-21 01:16:16 -0500

    fixing webpack issue

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import logo from "./react-logo.png"
+
+ function Page() {
+    return (
+        <div>
+            <header>
+                <nav>
+                    <img src={logo} width="40px" />
+                </nav>
+            </header>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
+            <footer>
+                <small>© 2021 Ziroll development. All rights reserved.</small>
+            </footer>
+        </div>
+    )
+}
+
+ReactDOM.render(<Page />, document.getElementById("app"));
